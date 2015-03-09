dc11d028fa5dff9f45794eb47ad0c1cd28cf6286
zouchongjin <zouchongjin@sina.com>
2015-03-09 14:19:30 +0800

    SpringMVC--支持两种常用时间格式的参数提交

--- src/main/java/com/zcj/web/springmvc/action/BasicAction.java
+++ src/main/java/com/zcj/web/springmvc/action/BasicAction.java
@@ -1,9 +1,10 @@
 package com.zcj.web.springmvc.action;
 
+import java.beans.PropertyEditorSupport;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
@@ -12,15 +13,34 @@ import com.zcj.web.dto.Page;
 public class BasicAction {
 
 	protected Page page = new Page();
-	
+
 	protected String RESULT_PAR_ERROR = "404";// 参数错误的返回值
 
 	// 处理参数中的日期格式
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), true));
+		binder.registerCustomEditor(Date.class, new DateEditor());
 	}
-	
+
+	public class DateEditor extends PropertyEditorSupport {
+		@Override
+		public void setAsText(String text) throws IllegalArgumentException {
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			Date date = null;
+			try {
+				date = format.parse(text);
+			} catch (ParseException e) {
+				format = new SimpleDateFormat("yyyy-MM-dd");
+				try {
+					date = format.parse(text);
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+			}
+			setValue(date);
+		}
+	}
+
 	public Page getPage() {
 		return page;
 	}
