bfe76f6ca14a1cab04d6086a2821e212e53c7db0
zouchongjin <zouchongjin@sina.com>
2015-11-19 14:39:53 +0800

    ......

--- src/main/java/com/zcj/util/UtilFile.java
+++ src/main/java/com/zcj/util/UtilFile.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -24,6 +25,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import javax.imageio.ImageIO;
@@ -37,6 +39,12 @@ import com.sun.image.codec.jpeg.JPEGEncodeParam;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
 import com.zcj.util.filenameutils.FilenameUtils;
 
+/**
+ * 文件相关操作（图片截图、压缩图片、下载文件、ZIP打包、删除文件和文件夹、获取MIME类型）
+ * 
+ * @author zouchongjin@sina.com
+ * @data 2015年11月19日
+ */
 public class UtilFile {
 
 	/** 图片文件类型数组 */
@@ -46,6 +54,16 @@ public class UtilFile {
 	/** 附件类型数组 */
 	public static final String[] fileType = { ".rar", ".doc", ".docx", ".zip", ".pdf", ".txt", ".swf", ".wmv", ".avi", ".rm", ".rmvb",
 			".mpeg", ".mpg", ".ogg", ".mov", ".wmv", ".mp4" };
+
+	public static Map<String, String> MIMETYPE_MAP = new HashMap<String, String>();
+
+	static {
+		MIMETYPE_MAP.put("doc", "application/msword");
+		MIMETYPE_MAP.put("js", "application/x-javascript");
+		MIMETYPE_MAP.put("css", "text/css");
+		MIMETYPE_MAP.put("png", "image/png");
+		MIMETYPE_MAP.put("apk", "application/vnd.android.package-archive");
+	}
 
 	/**
 	 * 截图
@@ -139,150 +157,7 @@ public class UtilFile {
 	}
 
 	/**
-	 * 压缩图片方法
-	 * 
-	 * @deprecated
-	 * 
-	 * @param oldFile
-	 *            将要压缩的图片
-	 * @param width
-	 *            压缩宽
-	 * @param height
-	 *            压缩高
-	 * @param quality
-	 *            压缩清晰度 <b>建议为1.0</b>
-	 * @param smallIcon
-	 *            压缩图片后,添加的扩展名（在图片后缀名前添加）
-	 * @param percentage
-	 *            是否等比压缩 若true宽高比率将将自动调整
-	 * @return 如果处理正确返回压缩后的文件名 null则参数可能有误
-	 */
-	public static String doCompress(String oldFile, int width, int height, float quality, String smallIcon, boolean percentage) {
-		if (oldFile != null && width > 0 && height > 0) {
-			Image srcFile = null;
-			String newImage = null;
-			try {
-				File file = new File(oldFile);
-				// 文件不存在
-				if (!file.exists()) {
-					return null;
-				}
-				/* 读取图片信息 */
-				srcFile = ImageIO.read(file);
-				int new_w = width;
-				int new_h = height;
-				if (percentage) {
-					// 为等比缩放计算输出的图片宽度及高度
-					double rate1 = ((double) srcFile.getWidth(null)) / (double) width + 0.1;
-					double rate2 = ((double) srcFile.getHeight(null)) / (double) height + 0.1;
-					double rate = rate1 > rate2 ? rate1 : rate2;
-					new_w = (int) (((double) srcFile.getWidth(null)) / rate);
-					new_h = (int) (((double) srcFile.getHeight(null)) / rate);
-				}
-				/* 宽高设定 */
-				BufferedImage tag = new BufferedImage(new_w, new_h, BufferedImage.TYPE_INT_RGB);
-				tag.getGraphics().drawImage(srcFile, 0, 0, new_w, new_h, null);
-				/* 压缩后的文件名 */
-				String filePrex = oldFile.substring(0, oldFile.lastIndexOf('.'));
-				newImage = filePrex + smallIcon + oldFile.substring(filePrex.length());
-				/* 压缩之后临时存放位置 */
-				FileOutputStream out = new FileOutputStream(newImage);
-				JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
-				JPEGEncodeParam jep = JPEGCodec.getDefaultJPEGEncodeParam(tag);
-				/* 压缩质量 */
-				jep.setQuality(quality, true);
-				encoder.encode(tag, jep);
-				out.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				srcFile.flush();
-			}
-			return newImage;
-		} else {
-			return null;
-		}
-	}
-
-	// UtilFile.doCompress("D:/JAVA/Software/eclipse/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/pwork/upload/zkgl/20130910154224163060.jpg",
-	// 200, 200, 1, "", false);
-
-	/**
-	 * 文件类型判断
-	 * 
-	 * @param fileName
-	 * @param imageFileTypes
-	 * @return
-	 */
-	private static boolean checkFileType(String fileName, String[] imageFileTypes) {
-		if (imageFileTypes == null) {
-			return true;
-		}
-		Iterator<String> type = Arrays.asList(imageFileTypes).iterator();
-		while (type.hasNext()) {
-			String ext = type.next();
-			if (fileName.toLowerCase().endsWith(ext)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * 依据原始文件名生成新文件名
-	 * 
-	 * @param fileName
-	 * @return
-	 */
-	private static String getName(String fileName) {
-		return UtilString.getSoleCode() + "." + FilenameUtils.getExtension(fileName);
-	}
-
-	private static void persistence(InputStream in, String fileName, String absoluteCatalog) {
-		File file = new File(absoluteCatalog);
-		if (!file.exists()) {
-			file.mkdirs();
-		}
-		FileOutputStream out = null;
-		FileChannel fout = null;
-		try {
-			out = new FileOutputStream(absoluteCatalog + fileName);
-			fout = out.getChannel();
-			byte[] buffers = new byte[1024 * 4];
-			int readLen = -1;
-			while ((readLen = in.read(buffers)) != -1) {
-				ByteBuffer buffer = ByteBuffer.wrap(buffers, 0, readLen);
-				fout.write(buffer);
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			throw new RuntimeException("文件不存在");
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new RuntimeException("文件写入出错");
-		} finally {
-			if (null != fout) {
-				try {
-					fout.close();
-					out.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			if (null != in) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-	/**
-	 * 下载图片（下载到项目运行环境的硬盘里）
+	 * 下载文件（下载到项目运行环境的硬盘里）
 	 * 
 	 * @param imgUrl
 	 *            图片地址，如http://www.baidu.com/a.jpg
@@ -290,9 +165,9 @@ public class UtilFile {
 	 *            保存的地址，如C:\\ABC.jpg
 	 * @throws IOException
 	 */
-	public static void download(String imgUrl, String path) throws IOException {
+	public static void download(String fileUrl, String path) throws IOException {
 		File file = new File(path);
-		URL url = new URL(imgUrl);
+		URL url = new URL(fileUrl);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		DataInputStream in = new DataInputStream(conn.getInputStream());
 		DataOutputStream out = new DataOutputStream(new FileOutputStream(file));
@@ -307,7 +182,7 @@ public class UtilFile {
 	}
 
 	/**
-	 * 压缩文件夹
+	 * 打包文件夹
 	 * 
 	 * @param inputFileName
 	 *            源文件夹目录 如D:\\TEMP\\ABC
@@ -339,6 +214,38 @@ public class UtilFile {
 			}
 			in.close();
 		}
+	}
+
+	/**
+	 * 解压（文件和文件夹的名称不能为中文）
+	 * 
+	 * @param zipFilePath
+	 *            源压缩包文件路径，如 C:\\ABC.zip
+	 * @param outputFilePath
+	 *            解压到指定的目录，如 C:\\ABC
+	 * @throws Exception
+	 */
+	public static void unZip(String zipFilePath, String outputFilePath) throws Exception {
+		ZipInputStream zin = new ZipInputStream(new FileInputStream(zipFilePath));
+		BufferedInputStream bin = new BufferedInputStream(zin);
+		File Fout = null;
+		ZipEntry entry;
+		while ((entry = zin.getNextEntry()) != null && !entry.isDirectory()) {
+			Fout = new File(outputFilePath, entry.getName());
+			if (!Fout.exists()) {
+				(new File(Fout.getParent())).mkdirs();
+			}
+			FileOutputStream out = new FileOutputStream(Fout);
+			BufferedOutputStream bout = new BufferedOutputStream(out);
+			int b;
+			while ((b = bin.read()) != -1) {
+				bout.write(b);
+			}
+			bout.close();
+			out.close();
+		}
+		bin.close();
+		zin.close();
 	}
 
 	/**
@@ -424,21 +331,12 @@ public class UtilFile {
 		}
 		return file;
 	}
-	
-	public static Map<String, String> MIMETYPE_MAP = new HashMap<String, String>();
-
-	static {
-		MIMETYPE_MAP.put("doc", "application/msword");
-		MIMETYPE_MAP.put("js", "application/x-javascript");
-		MIMETYPE_MAP.put("css", "text/css");
-		MIMETYPE_MAP.put("png", "image/png");
-		MIMETYPE_MAP.put("apk", "application/vnd.android.package-archive");
-	}
 
 	/**
 	 * 获取文件的MIME类型
+	 * 
 	 * @param filename
-	 * 			带后缀的文件名。支持：foo.txt、a/b/c.jpg等格式
+	 *            带后缀的文件名。支持：foo.txt、a/b/c.jpg等格式
 	 * @return
 	 */
 	public static String getMimeType(String filename) {
@@ -451,7 +349,7 @@ public class UtilFile {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 百度编辑器远程抓取图片到本地<br/>
 	 * window.UEDITOR_CONFIG.catchRemoteImageEnable = true;// 是否开启远程图片抓取,默认开启<br/>
@@ -663,5 +561,147 @@ public class UtilFile {
 		}
 	}
 
+	/**
+	 * 压缩图片方法
+	 * 
+	 * @deprecated
+	 * 
+	 * @param oldFile
+	 *            将要压缩的图片的物理地址
+	 * @param width
+	 *            压缩宽
+	 * @param height
+	 *            压缩高
+	 * @param quality
+	 *            压缩清晰度 <b>建议为1.0</b>
+	 * @param smallIcon
+	 *            压缩图片后,添加的扩展名（在图片后缀名前添加）
+	 * @param percentage
+	 *            是否等比压缩 若true宽高比率将将自动调整
+	 * @return 如果处理正确返回压缩后的文件名 null则参数可能有误
+	 */
+	public static String doCompress(String oldFile, int width, int height, float quality, String smallIcon, boolean percentage) {
+		if (oldFile != null && width > 0 && height > 0) {
+			Image srcFile = null;
+			String newImage = null;
+			try {
+				File file = new File(oldFile);
+				// 文件不存在
+				if (!file.exists()) {
+					return null;
+				}
+				/* 读取图片信息 */
+				srcFile = ImageIO.read(file);
+				int new_w = width;
+				int new_h = height;
+				if (percentage) {
+					// 为等比缩放计算输出的图片宽度及高度
+					double rate1 = ((double) srcFile.getWidth(null)) / (double) width + 0.1;
+					double rate2 = ((double) srcFile.getHeight(null)) / (double) height + 0.1;
+					double rate = rate1 > rate2 ? rate1 : rate2;
+					new_w = (int) (((double) srcFile.getWidth(null)) / rate);
+					new_h = (int) (((double) srcFile.getHeight(null)) / rate);
+				}
+				/* 宽高设定 */
+				BufferedImage tag = new BufferedImage(new_w, new_h, BufferedImage.TYPE_INT_RGB);
+				tag.getGraphics().drawImage(srcFile, 0, 0, new_w, new_h, null);
+				/* 压缩后的文件名 */
+				String filePrex = oldFile.substring(0, oldFile.lastIndexOf('.'));
+				newImage = filePrex + smallIcon + oldFile.substring(filePrex.length());
+				/* 压缩之后临时存放位置 */
+				FileOutputStream out = new FileOutputStream(newImage);
+				JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+				JPEGEncodeParam jep = JPEGCodec.getDefaultJPEGEncodeParam(tag);
+				/* 压缩质量 */
+				jep.setQuality(quality, true);
+				encoder.encode(tag, jep);
+				out.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				srcFile.flush();
+			}
+			return newImage;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * 文件类型判断
+	 * 
+	 * @param fileName
+	 * @param imageFileTypes
+	 * @return
+	 */
+	@Deprecated
+	private static boolean checkFileType(String fileName, String[] imageFileTypes) {
+		if (imageFileTypes == null) {
+			return true;
+		}
+		Iterator<String> type = Arrays.asList(imageFileTypes).iterator();
+		while (type.hasNext()) {
+			String ext = type.next();
+			if (fileName.toLowerCase().endsWith(ext)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 依据原始文件名生成新文件名
+	 * 
+	 * @param fileName
+	 * @return
+	 */
+	@Deprecated
+	private static String getName(String fileName) {
+		return UtilString.getSoleCode() + "." + FilenameUtils.getExtension(fileName);
+	}
+
+	@Deprecated
+	private static void persistence(InputStream in, String fileName, String absoluteCatalog) {
+		File file = new File(absoluteCatalog);
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+		FileOutputStream out = null;
+		FileChannel fout = null;
+		try {
+			out = new FileOutputStream(absoluteCatalog + fileName);
+			fout = out.getChannel();
+			byte[] buffers = new byte[1024 * 4];
+			int readLen = -1;
+			while ((readLen = in.read(buffers)) != -1) {
+				ByteBuffer buffer = ByteBuffer.wrap(buffers, 0, readLen);
+				fout.write(buffer);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("文件不存在");
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("文件写入出错");
+		} finally {
+			if (null != fout) {
+				try {
+					fout.close();
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (null != in) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 }
