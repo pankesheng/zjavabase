5075270b169820f27632892071f5c7077505c57b
zouchongjin <zouchongjin@sina.com>
2015-12-28 15:44:19 +0800

    ...

---
+++ src/main/java/com/zcj/util/coder/page/PageBuilder.java
@@ -0,0 +1,66 @@
+package com.zcj.util.coder.page;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.zcj.util.UtilString;
+
+public class PageBuilder {
+
+	public static PageBean initPage(Class<?> c) {
+		if (c.isAnnotationPresent(PageType.class)) {
+			PageBean p = new PageBean();
+
+			PageType pageType = c.getAnnotation(PageType.class);
+			p.setPname(pageType.pname());
+			p.setName(pageType.name());
+			p.setClassName(c.getSimpleName());
+			p.setModuleName(c.getName().split("\\.")[4]);
+			p.setDialog(pageType.dialog());
+			p.setUeditor(pageType.ueditor());
+			p.setExport(pageType.export());
+
+			List<PageColumnBean> columnList = new ArrayList<PageColumnBean>();
+			Field[] fs = c.getDeclaredFields();
+			for (Field f : fs) {
+				if (f.isAnnotationPresent(PageColumnType.class)) {
+					PageColumnBean b = new PageColumnBean();
+					PageColumnType ct = f.getAnnotation(PageColumnType.class);
+					if (UtilString.isNotBlank(ct.check())) {
+						b.setCheck(ct.check());
+					}
+					if (UtilString.isNotBlank(ct.defaultValue())) {
+						b.setDefaultValue(ct.defaultValue());
+					}
+					b.setFieldName(f.getName());
+					b.setGrid(ct.grid());
+					Map<String, String> kvMap = new LinkedHashMap<String, String>();
+					String[] kvArray = ct.keyValue();
+					if (kvArray != null && kvArray.length > 0) {
+						for (String kvString : kvArray) {
+							String[] kv2Array = kvString.split("=");
+							kvMap.put(kv2Array[0], kv2Array[1]);
+						}
+					}
+					b.setKeyValue(kvMap);
+					if (ct.maxlength() != 0) {
+						b.setMaxlength(ct.maxlength());
+					}
+					b.setModify(ct.modify());
+					b.setMust(ct.must());
+					b.setName(ct.name());
+					b.setSearch(ct.search());
+					b.setType(ct.type());
+					columnList.add(b);
+				}
+			}
+			p.setColumnList(columnList);
+			return p;
+		}
+		return null;
+	}
+
+}
