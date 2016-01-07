b0b8369e7871c5777f619cb135a1cb9c09dba905
zouchongjin <zouchongjin@sina.com>
2016-01-07 10:59:15 +0800

    ...

--- src/main/java/com/zcj/ext/jpush/JPushUtil.java
+++ src/main/java/com/zcj/ext/jpush/JPushUtil.java
@@ -20,7 +20,7 @@ import cn.jpush.api.push.model.notification.Notification;
 //	<groupId>cn.jpush.api</groupId>
 //	<artifactId>jpush-client</artifactId>
 //	<version>3.2.5</version>
-//</dependency>
+//</dependency>111
 public class JPushUtil {
 
 	private static Logger logger = Logger.getLogger(JPushUtil.class);
